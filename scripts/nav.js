53943c1f55edc35f7c7b6628225359980ab64f7e
Gavin Daniel <gavindaniel@email.arizona.edu>
2019-07-11 23:46:06 -0700

    added footer

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,15 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
+    // var options = {
+				// 	    edge: 'left',
+				// 	    draggable: true,
+				// 	    inDuration: 250,
+				// 	    outDuration: 200,
+				// 	    onOpenStart: null,
+				// 	    onOpenEnd: null,
+				// 	    onCloseStart: null,
+				// 	    onCloseEnd: null,
+				// 	    preventScrolling: true
+				// 	};
+
     // Since we are using default options we use {} instead of options
     // var instances = M.Sidenav.init(elems, options);
     var instances = M.Sidenav.init(elems, {});
   });
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+  // var collapsibleElem = document.querySelector('.collapsible');
+  // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
   // Or with jQuery
 
   // $(document).ready(function(){
   //   $('.sidenav').sidenav();
   // });
+
+
 
 // var html = '<nav class="transparent z-depth-0">';
 // html += '<div class="nav-wrapper">';
