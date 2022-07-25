1e0ffe60b4fc094aa5220fd60ff7e08ae5691c70
Gavin Daniel <gavindaniel@email.arizona.edu>
2022-07-24 18:52:03 -0700

    test

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
-let n = 0;
+// let n = 0;
 
-function addOne(){
-  n++;
-  document.getElementById("pill").innerHTML = n.toString();
-  document.getElementById("disp").value = n;
-}
-function subOne(){
-  if (n > 0) {  n--;  } else { n = 0; }
+// function addOne(){
+//   n++;
+//   document.getElementById("pill").innerHTML = n.toString();
+//   document.getElementById("disp").value = n;
+// }
+// function subOne(){
+//   if (n > 0) {  n--;  } else { n = 0; }
   
-  document.getElementById("pill").innerHTML = n.toString();
-  document.getElementById("disp").value = n;
-}
+//   document.getElementById("pill").innerHTML = n.toString();
+//   document.getElementById("disp").value = n;
+// }
